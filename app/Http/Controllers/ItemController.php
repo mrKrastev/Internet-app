57538d650dbb59f9c78abaf54136eafb64a11145
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2020-04-22 20:30:50 +0100

    edit and update implemented!

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -111,7 +111,8 @@ return view('items.show',compact('item'),['manyurls'=>$manyurls]);
      */
     public function edit($id)
     {
-        //
+      $item = Item::find($id);
+return view('items.edit',compact('item'));
     }
 
     /**
@@ -123,7 +124,53 @@ return view('items.show',compact('item'),['manyurls'=>$manyurls]);
      */
     public function update(Request $request, $id)
     {
-        //
+      $item=Item::find($id);
+      $this->validate(request(), [
+      'ItemName' => 'required',
+      'Category' => 'required',
+      'Colour' => 'required',
+      'Date' => 'required',
+      'Location' => 'required',
+      'Description' => 'required',
+      'Pictures[]' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:500',
+      ]);
+      //Handles the uploading of the image
+      $allImageNames='noimage.jpg';
+      if ($request->hasFile('Picture')){
+        $allImageNames='';
+        foreach($request->file('Picture') as $image){
+      //Gets the filename with the extension
+      $fileNameWithExt = $image->getClientOriginalName();
+      //just gets the filename
+      $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+      //Just gets the extension
+      $extension = $image->getClientOriginalExtension();
+      //Gets the filename to store
+      $fileNameToStore = $filename.'_'.time().'.'.$extension;
+      if ($allImageNames=='') {
+        $allImageNames=$fileNameToStore;
+      }else {
+      $allImageNames=$allImageNames.','.$fileNameToStore;
+      }
+      //Uploads the image
+      $path =$image->storeAs('public/images', $fileNameToStore);
+      }
+      }
+      else {
+      $fileNameToStore = 'noimage.jpg';
+      }
+      $item->ItemName = $request->input('ItemName');
+      $item->Category = $request->input('Category');
+      $item->Colour = $request->input('Colour');
+      $item->Date = $request->input('Date');
+      $item->Location = $request->input('Location');
+      $item->Description = $request->input('Description');
+      $item->updated_at = now();
+      $item->Pictures = $allImageNames;
+      // save the Vehicle object
+      $item->save();
+      // generate a redirect HTTP response with a success message
+      return back()->with('success', 'Item has been updated');
     }
 
     /**
---
+++ resources/views/items/edit.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-md-8 ">
+<div class="card">
+<div class="card-header">Edit and update the item</div>
+@if ($errors->any())
+<div class="alert alert-danger">
+<ul>
+@foreach ($errors->all() as $error)
+<li>{{ $error }}</li>
+@endforeach
+</ul>
+</div><br />
+@endif
+@if (\Session::has('success'))
+<div class="alert alert-success">
+<p>{{ \Session::get('success') }}</p>
+</div><br />
+@endif
+<div class="card-body">
+<form class="form-horizontal" method="POST" action="{{ action('ItemController@update',
+$item['id']) }} " enctype="multipart/form-data" >
+@method('PATCH')
+@csrf
+<div class="col-md-8">
+<label >Item Name</label>
+<input type="text" name="ItemName" value="{{$item->ItemName}}"/>
+</div>
+<div class="col-md-8">
+<label>Category</label>
+<select name="Category" value="{{ $item->Category }}">
+<option value="Jewellery">Jewellery</option>
+<option value="Pets">Pets</option>
+<option value="Electronic Devices">Electronic Devices</option>
+</select>
+</div>
+<div class="col-md-8">
+<label >Colour</label>
+<input type="text" name="Colour" value="{{$item->Colour}}" />
+</div>
+<div class="col-md-8">
+<label >Location</label>
+<input type="text" name="Location" value="{{$item->Location}}" />
+</div>
+<div class="col-md-8">
+<label >Date</label>
+<input type="datetime-local" id="birthdaytime" name="Date" value="{{$item->Date}}">
+</div>
+<div class="col-md-8">
+<label >Description</label>
+<textarea rows="4" cols="50" name="Description" > {{$item->Description}}
+</textarea>
+</div>
+<div class="col-md-8">
+<label>Image</label>
+<input type="file" name="Picture[]" multiple />
+</div>
+<div class="col-md-6 col-md-offset-4">
+<input type="submit" class="btn btn-primary" />
+<input type="reset" class="btn btn-primary" />
+</a>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
